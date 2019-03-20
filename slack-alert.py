9818b9d5aa78b86c1dc16b37f1d9477d17c6a045
Matt H <matth1117@tamu.edu>
2019-03-20 16:33:36 -0500

    Create slack-alert.py

---
+++ slack-alert.py
@@ -0,0 +1,11 @@
+import os
+from slackclient import SlackClient
+
+slack_token = os.environ["xoxb-522948478901-583357389586-3cWfxNKoXNjGWCmDOZDSgrGs"]
+sc = SlackClient(slack_token)
+
+sc.api_call(
+    "chat.postMessage",
+    chennel="#botspam-tcm",
+    text="TESTING -Matt"
+)
