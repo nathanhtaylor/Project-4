d4002aa47a5a3e37533109bfd4fccc8c491076db
stephanievchheng <47198017+stephanievchheng@users.noreply.github.com>
2019-03-21 12:42:12 -0500

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 def is_prime(num):
     if num < 2:
-        return "Enter number larger than 1"
+        return 'Enter number larger than 1'
     else:
         for x in range(2,num):
             if num % x == 0:
-               return "Not a prime"
-        return "Is a prime"
+               return 'Not a prime'
+        return 'Is a prime'
     #return is_prime 
 
 if __name__ == "__main__":
