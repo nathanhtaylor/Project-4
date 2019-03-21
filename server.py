456f75b2ceff413f864bf46f83e2f234be7b0357
matth1117 <47222177+matth1117@users.noreply.github.com>
2019-03-21 15:40:47 -0500

    Update server.py

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def send_slack(x):
     web_hook_url = 'https://hooks.slack.com/services/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'
 
     #x = 6
-    slack_msg = {'text': x }
+    slack_msg = {'text':x}
     requests.post(web_hook_url,data=json.dumps(slack_msg))
     return jsonify(
         input=x,
