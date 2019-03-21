a8d56bb4d98e0b14f87f9f50b97ba064178834cd
GullySock <47152780+GullySock@users.noreply.github.com>
2019-03-21 00:08:46 -0500

    Update Fibbonachi.py
    This just converts the input to an int, and sets the intial value for the fibonaci placeholder

--- Fibbonachi.py
+++ Fibbonachi.py
@@ -7,7 +7,12 @@ farray = []
 fnumraw = input("Give me a num ")
 
 if fnumraw.isdigit():
-    
+    fnum = int(fnumraw)
+    fplaceholder = fnew + fold    
+
+   
+
+    print (farray)
 
 else:
     print ("You must input a positive number")
