3e538511e75448dfc38e06ad5004a6998eb4a891
stephanievchheng <47198017+stephanievchheng@users.noreply.github.com>
2019-03-21 12:17:44 -0500

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -2,15 +2,15 @@ from flask import flask
 
 app = Flask(__name__)
 
-@app.route('/is-prime/<int>')
+@app.route('/is-prime/<num>')
 
-is_prime = 0
 def is_prime(num):
     for i in range(2, num):
         if n%i==0:
-            return False 
-    return True
-return is_prime 
+            return False
+        else:
+            return True
+    return is_prime  
 
 if __name__ == "__main__":
     app.debug = True 
