7de72c3f2d2cc9c008de469094374d47d0877b48
GullySock <47152780+GullySock@users.noreply.github.com>
2019-03-21 22:50:56 -0500

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,1 +1,31 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/factorial/<fnum>')
+def fibonacci(fnum):
+        
+
+    if fnum == "0":
+        return "1"
+
+    elif fnum.isdigit():
+        
+        fnum = int(fnum)
+        x = 1
+        sfnum = fnum
+        
+        while x < fnum:
+            sfnum = sfnum * x
+            x = x + 1
+            
+        sfnum = str(sfnum)
+        return sfnum
+    
+
+    else:
+        return "You must input a positive integer"
+ 
+      
+#app.debug = True
+app.run()
